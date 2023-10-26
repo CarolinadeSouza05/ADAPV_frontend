148473f3042ccbbabac5d768a7d4124a0d8ff070
Alerrando <alerrando2@gmail.com>
2023-10-26 08:57:57 -0300

    feat: inital css adm

--- src/Telas/Adm.jsx
+++ src/Telas/Adm.jsx
@@ -4,11 +4,15 @@ import { CgMenuGridO } from "react-icons/cg"
 export function Adm(){
     return (
         <main className="container-main-adm">
-            <aside className="aside-container-adm">
+            <aside className="container-aside-adm">
                 <div className="aside-menu-header">
                     <CgMenuGridO className="svg-menu-adm" />
                 </div>
             </aside>
+
+            <section className="container-dashboard-adm">
+                
+            </section>
         </main>
     )
 }
